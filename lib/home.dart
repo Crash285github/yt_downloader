04997e06b7d3fb31c10e928d1aed041231540222
Crash285 <jackadam852@gmail.com>
2024-03-17 22:17:06 +0100

    search function done

---
+++ lib/home.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late final YoutubeExplode _yt;
+  final List<Video> videos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _yt = YoutubeExplode();
+  }
+
+  @override
+  void dispose() {
+    _yt.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 64.0, horizontal: 32.0),
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return TextField(
+              onSubmitted: search,
+            );
+          }
+          return Text(videos[index - 1].title);
+        },
+        itemCount: videos.length + 1,
+      ),
+    );
+  }
+
+  void search(value) async {
+    setState(() => videos.clear());
+    final videoId = VideoId.parseVideoId(value);
+    if (videoId != null) {
+      final video = await _yt.videos.get(videoId);
+      setState(() => videos.add(video));
+      return;
+    }
+
+    final playlistId = PlaylistId.parsePlaylistId(value);
+    if (playlistId != null) {
+      final gotVideos = await _yt.playlists.getVideos(playlistId).toList();
+      setState(() => videos.addAll(gotVideos));
+    }
+  }
+}
